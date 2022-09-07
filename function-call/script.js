7443ba21b6be73f181ebe95c63b419cdd3005525
manirajvcnr <cnrakashcnr@gmail.com>
2022-09-07 17:05:57 +0530

    add-file.

---
+++ function-call/script.js
@@ -0,0 +1,7 @@
+// function call multiple
+
+function add(p1,p2){
+	console.log("The sum of two number is "+(p1+p2));
+}
+add(10,20);
+add(20,20);
---
+++ function-string/script.js
@@ -0,0 +1,8 @@
+//print string using function
+
+let str=prompt("enter the name");
+
+function string(strl){
+	console.log(strl);
+}
+string(str);
